504a463cae268c7049a2c50b8a70f4272a9423a2
Saathvika Ajith <saathvika.ajith@gmail.com>
2022-03-09 16:47:16 -0800

    code cleanup

--- main.py
+++ main.py
@@ -4,38 +4,11 @@ from flask import Flask, render_template
 # create a Flask instance
 app = Flask(__name__)
 
-# blueprint importing
-from routes.misc import app_misc
-
-# blueprint registration
-app.register_blueprint(app_misc)
-
 
 # connects default URL to render index.html
 @app.route('/')
 def index():
     return render_template("index.html")
-
-
-# connects /kangaroos path to render kangaroos.html
-@app.route('/kangaroos/')
-def kangaroos():
-    return render_template("kangaroos.html")
-
-
-@app.route('/walruses/')
-def walruses():
-    return render_template("walruses.html")
-
-
-@app.route('/hawkers/')
-def hawkers():
-    return render_template("hawkers.html")
-
-
-@app.route('/stub/')
-def stub():
-    return render_template("stub.html")
 
 
 # runs the application on the development server
