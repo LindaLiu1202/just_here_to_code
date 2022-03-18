7dbadd59f6eaa0e01959b0e5cd01a5065b0f313c
Daniel Tsivkovski <dtsivkovs@gmail.com>
2022-03-18 09:12:07 -0700

    added agnatural button + modal

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ app.register_blueprint(app_misc)
 def index():
     return render_template("index.html")
 
+@app.route('/industry-sectors/')
+def industry():
+    return render_template("CTE/industry-sectors/industrysectors.html")
 
 # runs the application on the development server
 if __name__ == "__main__":
