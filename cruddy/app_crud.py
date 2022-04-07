371d6233edf37595497e9581768e07927a8a58b7
LindaLiu1202 <lin.ltl.135135@gmail.com>
2022-04-07 14:42:50 -0700

    Add Phone Number to Sign Up screen

--- cruddy/app_crud.py
+++ cruddy/app_crud.py
@@ -60,8 +60,9 @@ def crud_authorize():
         user_name = request.form.get("user_name")
         email = request.form.get("email")
         password1 = request.form.get("password1")
-        password2 = request.form.get("password1")           # password should be verified
-        if authorize(user_name, email, password1):    # zero index [0] used as user_name and email are type tuple
+        password2 = request.form.get("password1") # password should be verified
+        phone = request.form.get("phone")
+        if authorize(user_name, email, password1, phone):    # zero index [0] used as user_name and email are type tuple
             return redirect(url_for('crud.crud_login'))
     # show the auth user page if the above fails for some reason
     return render_template("authorize.html")
--- cruddy/query.py
+++ cruddy/query.py
@@ -94,7 +94,7 @@ def user_loader(user_id):
 
 
 # Authorise new user requires user_name, email, password
-def authorize(name, email, password):
+def authorize(name, email, password, phone):
     if is_user(email, password):
         return False
     else:
@@ -102,7 +102,7 @@ def authorize(name, email, password):
             name=name,
             email=email,
             password=password,
-            phone="1234567890"  # this should be added to authorize.html
+            phone=phone  # this should be added to authorize.html
         )
         # encrypt their password and add it to the auth_user object
         auth_user.create()
